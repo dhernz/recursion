640054009480713a29db6b0d80f112f8a28b80c0
Doris Hernandez <d.hernz@gmail.com>
2015-06-28 20:17:23 -0700

    adding stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,77 @@
 
 // but you don't so you're going to write it from scratch:
 
+// recursive reimplementation of JSON.stringify
 var stringifyJSON = function(obj) {
-  // your code goes here
+ 
+  // null
+  if (obj === null) {
+
+    return "null";
+  
+  }
+ 
+
+  if (obj === undefined ){ //check in if de object is defined
+
+    return undefined;
+  }
+
+
+  if(obj.constructor === Function) { 
+
+  	return  undefined;
+  }
+ 
+  // strings
+  if (obj.constructor === String) {
+    return '"' + obj + '"';
+  }
+ 
+  
+  if (obj.constructor === Array) {
+    if (obj.length) {
+      var partialJSON = [];
+
+      for (var i = 0; i < obj.length; i++) {
+        partialJSON.push(stringifyJSON(obj[i])); // recursion
+      }
+
+      return '[' + partialJSON.join(",") + ']';
+    } 
+    else {
+      return '[]';
+    }
+  }
+ 
+    // objects
+  if (obj.constructor === Object) {
+    var keys = Object.keys(obj); // converting an array of the keys the object ex:  [1,2,3]
+    if (keys.length) {
+      var partialJSON = '';
+  
+      for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+  
+        if (!key || obj[key] === undefined || typeof key === 'function' || typeof obj[key] === 'function') {
+  
+        } else {
+          if (i === keys.length - 1) {
+            partialJSON += stringifyJSON(key) + ':' + stringifyJSON(obj[key]); // recursion
+          } else {
+            partialJSON += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','; // recursion
+          }
+        }
+      }
+      return '{' + partialJSON + '}';
+    } else {
+      return '{}';
+    }
+  }
+ 
+  // everything else (numbers, booleans, etc.)
+  return obj.toString();
+ 
 };
+
+
